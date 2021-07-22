98c9e28b2e823a7a87d7164db35c671a6720fe4e
Ofir Naor <ofirnk@users.noreply.github.com>
2021-07-22 10:23:10 +0300

    Run pickle core tests only

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,15 @@
 import pytest
 # import shutil
 
-from .test_mongo_core import _test_mongetter
+# from .test_mongo_core import _test_mongetter
 # from cachier.pickle_core import EXPANDED_CACHIER_DIR
 
 
 def mongo_finalizer():
     """The finalizer for MongoCore-related tests."""
     print('\n Tearing down MongoCore-related assets...')
-    collection = _test_mongetter()
-    collection.drop_indexes()
+    # collection = _test_mongetter()
+    # collection.drop_indexes()
     print(" - Indexes dropped from cachier collection.")
 
 
